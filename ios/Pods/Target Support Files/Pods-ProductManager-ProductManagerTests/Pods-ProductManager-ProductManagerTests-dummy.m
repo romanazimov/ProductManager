69e232293ef0326d4f003f6031eda3b084379486
Roman <36523168+romanazimov@users.noreply.github.com>
2020-07-24 14:28:21 -0400

    1

---
+++ ios/Pods/Target Support Files/Pods-ProductManager-ProductManagerTests/Pods-ProductManager-ProductManagerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProductManager_ProductManagerTests : NSObject
+@end
+@implementation PodsDummy_Pods_ProductManager_ProductManagerTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-ProductManager-tvOS/Pods-ProductManager-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProductManager_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_ProductManager_tvOS
+@end
